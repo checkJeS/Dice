35e88558095a4e755ffc2280b45d72b947fa8ea3
checkJeS <lopezjesica38@gmail.com>
2017-03-30 23:00:27 -0700

    Add files via upload

---
+++ diceGameScript.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+	document.getElementById("btn1").onclick = function(){
+		
+		var random1 = Math.floor((Math.random()*6)+1);
+		dice1= parseInt ('random1').value;
+	}
+}
