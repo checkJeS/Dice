44ef61a9d56397f002a3f05dce0db26996d57b1c
checkJeS <lopezjesica38@gmail.com>
2017-03-30 23:01:37 -0700

    Update diceGameScript.js

--- diceGameScript.js
+++ diceGameScript.js
@@ -2,6 +2,6 @@ window.onload = function(){
 	document.getElementById("btn1").onclick = function(){
 		
 		var random1 = Math.floor((Math.random()*6)+1);
-		dice1= parseInt ('random1').value;
+		random1= parseInt ('random1').value;
 	}
 }
